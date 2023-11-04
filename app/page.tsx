027bfe650419ea7717a63701abbfbd805ec15325
NekoJar <mzharfanhakim7@gmail.com>
2023-11-04 21:04:49 +0700

    accessing session in the Server

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import ProductCard from "./users/components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1 className="pb-4">Home</h1>
+      <h1 className="pb-4">
+        Hello, {session && <span>{session.user!.name}</span>} 👋
+      </h1>
       <ProductCard />
     </main>
   );
