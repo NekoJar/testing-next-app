dff0b2f5d6e5753436ef423498396743331b4444
NekoJar <mzharfanhakim7@gmail.com>
2023-11-04 23:31:50 +0700

    styling emails page

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -3,22 +3,26 @@ import {
   Container,
   Html,
   Link,
+  Tailwind,
   Preview,
   Text,
 } from "@react-email/components";
 import React from "react";
+
 const WelcomeTemplate = ({ name }: { name: String }) => {
   return (
     <Html>
       <Preview>Welcome abroad!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-            Goodies
-          </Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+              Goodies
+            </Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
