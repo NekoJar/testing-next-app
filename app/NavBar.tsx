ac9db065ac6b3aef6570569eac60dfcbf3825516
NekoJar <mzharfanhakim7@gmail.com>
2023-11-04 19:30:22 +0700

    configuring google provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,16 +3,17 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className=" bg-slate-200 flex p-5 justify-end items-center">
-      <Link href="/" className="mr-5 ">
-        Home
-      </Link>
-      <Link href="/products" className="mr-5">
-        Products
-      </Link>
-      <Link href="/users" className="mr-5">
-        Users
-      </Link>
+    <div className=" bg-slate-200 flex p-5 justify-between items-center ">
+      <div className="ml-3">
+        <Link href="/" className=" font-bold uppercase text-lg">
+          Next App
+        </Link>
+      </div>
+      <div className="space-x-5 mr-3">
+        <Link href="/products">Products</Link>
+        <Link href="/users">Users</Link>
+        <Link href="/api/auth/signin">Login</Link>
+      </div>
     </div>
   );
 };
