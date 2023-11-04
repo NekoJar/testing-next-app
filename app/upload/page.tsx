182ceecafe2fc4c41b199a90049702fa863453e7
NekoJar <mzharfanhakim7@gmail.com>
2023-11-04 19:01:09 +0700

    customize the uploader box styles

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -15,6 +15,35 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="e1cy13w4"
+        options={{
+          sources: ["local", "camera", "google_drive", "image_search"],
+          multiple: false,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#000000",
+              sourceBg: "#000000",
+              windowBorder: "#8E9FBF",
+              tabIcon: "#FFFFFF",
+              inactiveTabIcon: "#8E9FBF",
+              menuIcons: "#2AD9FF",
+              link: "#08C0FF",
+              action: "#336BFF",
+              inProgress: "#00BFFF",
+              complete: "#33ff00",
+              error: "#EA2727",
+              textDark: "#000000",
+              textLight: "#FFFFFF",
+            },
+            fonts: {
+              default: null,
+              "'Poppins', sans-serif": {
+                url: "https://fonts.googleapis.com/css?family=Poppins",
+                active: true,
+              },
+            },
+          },
+        }}
         onUpload={(result, widget) => {
           if (result.event !== "success") return;
           const info = result.info as CloudinaryResult;
